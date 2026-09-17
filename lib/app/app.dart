76d26becc3c0480d680a8d65bee12beae854e957
royalguard14 <95269746+royalguard14@users.noreply.github.com>
2026-09-17 15:00:36 +0800

    refactor: use global app branding at startup

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../core/config/env.dart';
 import '../features/cart/providers/cart_provider.dart';
 import '../features/developer/providers/brand_theme_provider.dart';
 import '../features/splash/splash_screen.dart';
-import 'theme.dart';
 
 class HalalFoodApp extends StatelessWidget {
   const HalalFoodApp({super.key});
@@ -16,14 +14,14 @@ class HalalFoodApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => CartProvider()),
         ChangeNotifierProvider(
-          create: (_) => BrandThemeProvider()..load(Env.brandKey),
+          create: (_) => BrandThemeProvider()..load(),
         ),
       ],
       child: Consumer<BrandThemeProvider>(
         builder: (context, brandTheme, _) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            title: brandTheme.config?.appName ?? Env.appName,
+            title: brandTheme.config?.appName ?? 'HALAL Food',
             theme: brandTheme.theme,
             home: const SplashScreen(),
           );
